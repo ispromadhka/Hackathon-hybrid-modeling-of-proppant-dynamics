0b5611b3edcdd29a77978fbbdcceb2fcecd13b53
Folomkin Ivan <folomkin.ivan@yandex.ru>
2026-01-26 20:12:48 -0800

    Add changes

--- src/solver/t.py
+++ src/solver/t.py
@@ -1,16 +1,10 @@
-c_min, c_max, c_step = 0.05, 0.45, 0.05
-w_min, w_max, w_step = 0.01, 0.03, 0.005
-mu_min, mu_max, mu_step = 0.001, 0.1, 0.005
-q_min, q_max, q_step = 0.01, 0.5, 0.05
-chi_min, chi_max, chi_step = 15, 30, 3
-time_min, time_max, time_step = 50, 200, 25
-dT_values = [2]
+import numpy as np
+def load_timeseries(filepath):
+    data = np.load(filepath, allow_pickle=True)
+    Q = data['Q']
+    times = data['times']
+    return Q, times
 
-c_in = np.arange(c_min, c_max + c_step/2, c_step)
-w0 = np.arange(w_min, w_max + w_step/2, w_step)
-mu0 = np.arange(mu_min, mu_max + mu_step/2, mu_step)
-Q = -np.arange(q_min, q_max + q_step/2, q_step)
-chi = np.arange(chi_min, chi_max + chi_step/2, chi_step)
-c_in_times_list = np.arange(time_min, time_max + time_step/2, time_step)
-
-param_combinations = list(itertools.product(c_in, w0, mu0, Q, chi, c_in_times_list, dT_values))
+path = r'C:\Users\OneVay\Documents\hack\Hackathon-hybrid-modeling-of-proppant-dynamics\simulation_timeseries\c0.050_w0.010_mu0.001_Q-0.010_chi15.0_t50_dT2.0_series.npz'
+f = load_timeseries(path)
+print(f)
---
+++ torch_data/load.py
@@ -0,0 +1,3 @@
+import torch
+y = torch.load("./torch_data/data.pt")
+print(y)
